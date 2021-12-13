8b3550c63b5d9e92b77f55b3d821d0a46b24675b
Pete Dietl <petedietl@gmail.com>
2021-12-13 09:58:28 +0100

    drivers: adc: stm32: Disable ADC before calibration
    The STM32 docs state that the ADC may not be calibrated unless
    the ADC is disabled (ADEN=0). This commit implements this constraint
    
    Fixes #40936
    
    Signed-off-by: Pete Dietl <petedietl@gmail.com>

--- drivers/adc/adc_stm32.c
+++ drivers/adc/adc_stm32.c
@@ -319,49 +319,6 @@ static void adc_stm32_calib(const struct device *dev)
 }
 #endif
 
-/*
- * Enable ADC peripheral, and wait until ready if required by SOC.
- */
-static int adc_stm32_enable(ADC_TypeDef *adc)
-{
-#if defined(CONFIG_SOC_SERIES_STM32L4X) || \
-	defined(CONFIG_SOC_SERIES_STM32L5X) || \
-	defined(CONFIG_SOC_SERIES_STM32WBX) || \
-	defined(CONFIG_SOC_SERIES_STM32G0X) || \
-	defined(CONFIG_SOC_SERIES_STM32G4X) || \
-	defined(CONFIG_SOC_SERIES_STM32WLX)
-
-	if (LL_ADC_IsEnabled(adc) == 1UL) {
-		return 0;
-	}
-
-	LL_ADC_ClearFlag_ADRDY(adc);
-	LL_ADC_Enable(adc);
-
-	/*
-	 * Enabling ADC modules in L4, WB, G0 and G4 series may fail if they are
-	 * still not stabilized, this will wait for a short time to ensure ADC
-	 * modules are properly enabled.
-	 */
-	uint32_t count_timeout = 0;
-
-	while (LL_ADC_IsActiveFlag_ADRDY(adc) == 0) {
-		if (LL_ADC_IsEnabled(adc) == 0UL) {
-			LL_ADC_Enable(adc);
-			count_timeout++;
-			if (count_timeout == 10) {
-				return -ETIMEDOUT;
-			}
-		}
-	}
-#else
-	LL_ADC_Enable(adc);
-#endif
-
-	return 0;
-}
-
-
 static int start_read(const struct device *dev,
 		      const struct adc_sequence *sequence)
 {
@@ -452,10 +409,9 @@ static int start_read(const struct device *dev,
 		return err;
 	}
 
-#if defined(CONFIG_SOC_SERIES_STM32G0X) || \
-	defined(CONFIG_SOC_SERIES_STM32WLX)
+#if defined(CONFIG_SOC_SERIES_STM32G0X)
 	/*
-	 * Writing ADC_CFGR1 register while ADEN bit is set
+	 * Errata: Writing ADC_CFGR1 register while ADEN bit is set
 	 * resets RES[1:0] bitfield. We need to disable and enable adc.
 	 */
 	if (LL_ADC_IsEnabled(adc) == 1UL) {
@@ -464,14 +420,15 @@ static int start_read(const struct device *dev,
 	while (LL_ADC_IsEnabled(adc) == 1UL) {
 	}
 	LL_ADC_SetResolution(adc, resolution);
-	adc_stm32_enable(adc);
+	LL_ADC_Enable(adc);
+	while (LL_ADC_IsActiveFlag_ADRDY(adc) != 1UL) {
+	}
 #elif !defined(CONFIG_SOC_SERIES_STM32F1X) && \
 	!defined(STM32F3X_ADC_V2_5)
 	LL_ADC_SetResolution(adc, resolution);
 #endif
 
-#if defined(CONFIG_SOC_SERIES_STM32L0X) || \
-	defined(CONFIG_SOC_SERIES_STM32WLX)
+#ifdef CONFIG_SOC_SERIES_STM32L0X
 	/*
 	 * setting OVS bits is conditioned to ADC state: ADC must be disabled
 	 * or enabled without conversion on going : disable it, it will stop
@@ -567,11 +524,11 @@ static int start_read(const struct device *dev,
 		break;
 	default:
 		LOG_ERR("Invalid oversampling");
-		adc_stm32_enable(adc);
+		LL_ADC_Enable(adc);
 		return -EINVAL;
 	}
 	/* re-enable ADC after changing the OVS */
-	adc_stm32_enable(adc);
+	LL_ADC_Enable(adc);
 #else
 	if (sequence->oversampling) {
 		LOG_ERR("Oversampling not supported");
@@ -593,7 +550,7 @@ static int start_read(const struct device *dev,
 		}
 		adc_stm32_calib(dev);
 		/* re-enable ADC after calibration */
-		adc_stm32_enable(adc);
+		LL_ADC_Enable(adc);
 #else
 		LOG_ERR("Calibration not supported");
 		return -ENOTSUP;
@@ -614,7 +571,7 @@ static int start_read(const struct device *dev,
 #elif defined(CONFIG_SOC_SERIES_STM32F1X)
 	LL_ADC_EnableIT_EOS(adc);
 #elif defined(STM32F3X_ADC_V2_5)
-	adc_stm32_enable(adc);
+	LL_ADC_Enable(adc);
 	LL_ADC_EnableIT_EOS(adc);
 #else
 	LL_ADC_EnableIT_EOCS(adc);
@@ -954,10 +911,32 @@ static int adc_stm32_init(const struct device *dev)
 	}
 #endif
 
-	err = adc_stm32_enable(adc);
-	if (err < 0) {
-		return err;
+	LL_ADC_Enable(adc);
+
+#if defined(CONFIG_SOC_SERIES_STM32L4X) || \
+	defined(CONFIG_SOC_SERIES_STM32L5X) || \
+	defined(CONFIG_SOC_SERIES_STM32WBX) || \
+	defined(CONFIG_SOC_SERIES_STM32G0X) || \
+	defined(CONFIG_SOC_SERIES_STM32G4X) || \
+	defined(CONFIG_SOC_SERIES_STM32H7X) || \
+	defined(CONFIG_SOC_SERIES_STM32WLX)
+	/*
+	 * Enabling ADC modules in L4, WB, G0 and G4 series may fail if they are
+	 * still not stabilized, this will wait for a short time to ensure ADC
+	 * modules are properly enabled.
+	 */
+	uint32_t countTimeout = 0;
+
+	while (LL_ADC_IsActiveFlag_ADRDY(adc) == 0) {
+		if (LL_ADC_IsEnabled(adc) == 0UL) {
+			LL_ADC_Enable(adc);
+			countTimeout++;
+			if (countTimeout == 10) {
+				return -ETIMEDOUT;
+			}
+		}
 	}
+#endif
 
 	config->irq_cfg_func();
 
