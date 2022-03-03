46c4480ce56db500703585f671d6b153b995d6f0
Azam Lukman <azamlukmanabdullah@gmail.com>
2022-03-03 15:30:25 +0800

    add NPK Sensor channel

--- include/drivers/sensor.h
+++ include/drivers/sensor.h
@@ -176,6 +176,8 @@ enum sensor_channel {
 	SENSOR_CHAN_GAUGE_DESIRED_VOLTAGE,
 	/** Desired charging current in mA */
 	SENSOR_CHAN_GAUGE_DESIRED_CHARGING_CURRENT,
+	/** NPK (Nitrogen,phosphorus and potassium) content in soil**/
+	SENSOR_CHAN_SOIL_NPK,
 
 	/** All channels. */
 	SENSOR_CHAN_ALL,
