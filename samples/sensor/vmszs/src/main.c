f1380f66cd79f2db10f30ba0d09956eaa838c91f
Azam Lukman <azamlukmanabdullah@gmail.com>
2022-04-27 16:14:43 +0800

    Update main.c

--- samples/sensor/vmszs/src/main.c
+++ samples/sensor/vmszs/src/main.c
@@ -21,6 +21,7 @@
 #include <sys/util.h>
 #include <inttypes.h>
 #include <drivers/sensor/kx022.h>
+#include <sys/crc.h>
 
 #define SW0_NODE	DT_ALIAS(sw0)
 #if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
@@ -284,23 +285,27 @@ void accel_rdry_handler(void)
 		+
 		pow(sensor_value_to_double(&axis[1]),2)
 		+
-		pow(sensor_value_to_double(&axis[1]),2);
+		pow(sensor_value_to_double(&axis[2]) - 9.8 ,2);
 
-	v[2] = sqrt(v[1]);//*time;
+	v[2] = sqrt(v[1])/(time*MSEC_PER_SEC);
 
-	v[1] =(sensor_value_to_double(&axis[1])*9806.65)/314.16;
-	v[0] =(sensor_value_to_double(&axis[0])*9806.65)/314.16;
-	test =(sensor_value_to_double(&axis[2])*9806.65)/314.16;
-	v[2] = (v[2]  *9806.65)/314.16;
+	// v[1] =(sensor_value_to_double(&axis[1])*9806.65)/314.16;
+	// v[0] =(sensor_value_to_double(&axis[0])*980665)/314.16;
+	// test =(sensor_value_to_double(&axis[2])*9806.65)/314.16;
+	// v[2] = (v[2]  *9806.65)/314.16;
 	// if(fabsf(v[1])>fabsf(test))
 	// {
 	// 	test = v[1];
 	// }
-	printf("VX:%f\tVY:%f\tVZ:%f\tV2:%f\r\n",v[0],v[1],test,v[2]);
+	// printf("/*VX:%f\tVY:%f\tVZ:%f\tV2:%fmm/s\tdiff:%f*/\r\n",
+	// sqrt(pow(sensor_value_to_double(&axis[0]),2) /(time*MSEC_PER_SEC)),
+	// sqrt(pow(sensor_value_to_double(&axis[1]),2) /(time*MSEC_PER_SEC)),
+	// sqrt(pow(sensor_value_to_double(&axis[2]) - 9.8,2) /(time*MSEC_PER_SEC)),
+	// v[2]*1000,v[2]*1000 -test);
 
 
 	// printf("vx :%f\tvy:%f\tvz:%f\r\n",ctx.v_peak[0],ctx.v_peak[1],ctx.v_peak[2]);
-
+	test = v[2] *1000;
 	dt = k_uptime_get_32();
 	drdy.x[i] = (int)(sensor_value_to_double(&axis[0]) *10000);
 	drdy.y[i] = (int)(sensor_value_to_double(&axis[1]) *10000);
@@ -361,17 +366,17 @@ void accel_timer_handler(void)
 				if(xfer_data.x[size-count] >steady.x)
 				{
 					steady.x = xfer_data.x[size-count];
-					printf("\rsteady state x: %d\r\n",steady.x);
+					// printf("\rsteady state x: %d\r\n",steady.x);
 				}
 				if(xfer_data.y[size-count] >steady.y)
 				{
 					steady.y = xfer_data.y[size-count];
-					printf("\rsteady state y: %d\r\n",steady.y);
+					// printf("\rsteady state y: %d\r\n",steady.y);
 				}
 				if((fabs)(xfer_data.z[size-count])>steady.z)
 				{
 					steady.z = (fabs)(xfer_data.z[size-count]);
-					printf("\rsteady state z: %d\r\n",steady.z);
+					// printf("\rsteady state z: %d\r\n",steady.z);
 				}
 			}
 			STEADY_STATE_CALIBRATE = true;
@@ -525,7 +530,7 @@ void accel_work(struct k_work *work)
 	// printf("sensor value %f, round up %f\r\n",sensor_value_to_double(&ag[0]),rx);
 	if(first_data == false )
 	{
-		printf("Calibrating %d.....\r\n",cal);
+		// printf("Calibrating %d.....\r\n",cal);
 
 		last_axis.x += sensor_value_to_double(&ag[0]);
 		last_axis.y += sensor_value_to_double(&ag[1]);
@@ -669,15 +674,93 @@ void sht3x(void)
 
 
 }
+
+static uint16_t checksum(const uint8_t *data, uint8_t rd_size)
+{
+	static uint16_t table[256] = {
+		0x0000, 0xC0C1, 0xC181, 0x0140, 0xC301, 0x03C0, 0x0280, 0xC241, 0xC601, 0x06C0,
+		0x0780, 0xC741, 0x0500, 0xC5C1, 0xC481, 0x0440, 0xCC01, 0x0CC0, 0x0D80, 0xCD41,
+		0x0F00, 0xCFC1, 0xCE81, 0x0E40, 0x0A00, 0xCAC1, 0xCB81, 0x0B40, 0xC901, 0x09C0,
+		0x0880, 0xC841, 0xD801, 0x18C0, 0x1980, 0xD941, 0x1B00, 0xDBC1, 0xDA81, 0x1A40,
+		0x1E00, 0xDEC1, 0xDF81, 0x1F40, 0xDD01, 0x1DC0, 0x1C80, 0xDC41, 0x1400, 0xD4C1,
+		0xD581, 0x1540, 0xD701, 0x17C0, 0x1680, 0xD641, 0xD201, 0x12C0, 0x1380, 0xD341,
+		0x1100, 0xD1C1, 0xD081, 0x1040, 0xF001, 0x30C0, 0x3180, 0xF141, 0x3300, 0xF3C1,
+		0xF281, 0x3240, 0x3600, 0xF6C1, 0xF781, 0x3740, 0xF501, 0x35C0, 0x3480, 0xF441,
+		0x3C00, 0xFCC1, 0xFD81, 0x3D40, 0xFF01, 0x3FC0, 0x3E80, 0xFE41, 0xFA01, 0x3AC0,
+		0x3B80, 0xFB41, 0x3900, 0xF9C1, 0xF881, 0x3840, 0x2800, 0xE8C1, 0xE981, 0x2940,
+		0xEB01, 0x2BC0, 0x2A80, 0xEA41, 0xEE01, 0x2EC0, 0x2F80, 0xEF41, 0x2D00, 0xEDC1,
+		0xEC81, 0x2C40, 0xE401, 0x24C0, 0x2580, 0xE541, 0x2700, 0xE7C1, 0xE681, 0x2640,
+		0x2200, 0xE2C1, 0xE381, 0x2340, 0xE101, 0x21C0, 0x2080, 0xE041, 0xA001, 0x60C0,
+		0x6180, 0xA141, 0x6300, 0xA3C1, 0xA281, 0x6240, 0x6600, 0xA6C1, 0xA781, 0x6740,
+		0xA501, 0x65C0, 0x6480, 0xA441, 0x6C00, 0xACC1, 0xAD81, 0x6D40, 0xAF01, 0x6FC0,
+		0x6E80, 0xAE41, 0xAA01, 0x6AC0, 0x6B80, 0xAB41, 0x6900, 0xA9C1, 0xA881, 0x6840,
+		0x7800, 0xB8C1, 0xB981, 0x7940, 0xBB01, 0x7BC0, 0x7A80, 0xBA41, 0xBE01, 0x7EC0,
+		0x7F80, 0xBF41, 0x7D00, 0xBDC1, 0xBC81, 0x7C40, 0xB401, 0x74C0, 0x7580, 0xB541,
+		0x7700, 0xB7C1, 0xB681, 0x7640, 0x7200, 0xB2C1, 0xB381, 0x7340, 0xB101, 0x71C0,
+		0x7080, 0xB041, 0x5000, 0x90C1, 0x9181, 0x5140, 0x9301, 0x53C0, 0x5280, 0x9241,
+		0x9601, 0x56C0, 0x5780, 0x9741, 0x5500, 0x95C1, 0x9481, 0x5440, 0x9C01, 0x5CC0,
+		0x5D80, 0x9D41, 0x5F00, 0x9FC1, 0x9E81, 0x5E40, 0x5A00, 0x9AC1, 0x9B81, 0x5B40,
+		0x9901, 0x59C0, 0x5880, 0x9841, 0x8801, 0x48C0, 0x4980, 0x8941, 0x4B00, 0x8BC1,
+		0x8A81, 0x4A40, 0x4E00, 0x8EC1, 0x8F81, 0x4F40, 0x8D01, 0x4DC0, 0x4C80, 0x8C41,
+		0x4400, 0x84C1, 0x8581, 0x4540, 0x8701, 0x47C0, 0x4680, 0x8641, 0x8201, 0x42C0,
+		0x4380, 0x8341, 0x4100, 0x81C1, 0x8081, 0x4040
+	};
+
+	uint8_t xor = 0;
+	uint16_t crc = 0xFFFF;
+
+	while (rd_size--) {
+		xor = (*data++) ^ crc;
+		crc >>= 8;
+		crc ^= table[xor];
+	}
+
+	return crc;
+}
+
+static uint16_t MODBUS_CRC16_v2( const unsigned char *buf, unsigned int len )
+{
+	static const uint16_t table[2] = { 0x0000, 0xA001 };
+	uint16_t crc = 0xFFFF;
+	unsigned int i = 0;
+	char bit = 0;
+	unsigned int xor = 0;
+
+	for( i = 0; i < len; i++ )
+	{
+		crc ^= buf[i];
+
+		for( bit = 0; bit < 8; bit++ )
+		{
+			xor = crc & 0x01;
+			crc >>= 1;
+			crc ^= table[xor];
+		}
+	}
+
+	return crc;
+}
+
+
+int modbus_test(uint16_t dev_id,uint16_t addr ,uint16_t size)
+{
+	uint8_t convert[4];
+	uint8_t test[6]={dev_id,0x03,(addr>>8 &0xff),(addr>>0 &0xff),(size>>8 &0xff),(size>>0 &0xff)};
+	// uint8_t test[6] = { 0x04, 0x03, 0x00, 0x00, 0x00, 0x01};
+	return MODBUS_CRC16_v2(&test,sizeof(test));
+}
+
+
 int main(void)
 {
 	struct sensor_trigger trig;
 	struct sensor_trigger trig_motion;
 	uint8_t rc;
+	int16_t crc;
 	const struct device *sensor = device_get_binding("KX022");
 	const struct device *sensor1 = device_get_binding("KX022_2");
 	double_t lastmedX,lastmedY,lastmedZ;
-
+	
 	// int j =0;
 	// uint8_t gettype;
 	// uint32_t getdata[3];
@@ -712,6 +795,9 @@ int main(void)
 
 	while(1)
 	{
+		crc =modbus_test(4,0x0000,0x0001);
+
+	printf("crc %x\r\c",crc);
 
 			// sound();
 			// sht3x();
@@ -792,7 +878,7 @@ int main(void)
 		// 	lastmedX =ctx.medianX;
 		// 	lastmedY =ctx.medianY;
 		// 	lastmedZ = ctx.medianZ;
-		// 	// k_sleep(K_MSEC(500));
+			// k_sleep(K_MSEC(500));
 		// }
 	}
 }
