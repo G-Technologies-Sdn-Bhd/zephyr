05a7f4f8149464b9bfd603e091000839a2b2a950
syafiqq <syafiqafiq99@gmail.com>
2023-01-27 10:22:54 +0800

    add i2c bus status

--- drivers/sensor/pasco2/pasco2.c
+++ drivers/sensor/pasco2/pasco2.c
@@ -14,112 +14,96 @@
 #include <sys/byteorder.h>
 #include <sys/__assert.h>
 #include <logging/log.h>
+#include <string.h>
 
 #include "pasco2.h"
+#if DT_ANY_INST_ON_BUS_STATUS_OKAY(i2c)
+
+static int pasco2_i2c_read_data(const struct device *dev, uint8_t reg_addr, uint8_t *value,
+			       uint8_t len)
+{
+	const struct pasco2_config *cfg = dev->config;
+
+	return i2c_burst_read_dt(&cfg->bus, reg_addr, value, len);
+}
+
+static int pasco2_i2c_read_reg(const struct device *dev, uint8_t reg_addr, uint8_t *value)
+{
+	const struct pasco2_config *cfg = dev->config;
+
+	return i2c_reg_read_byte_dt(&cfg->bus, reg_addr, value);
+}
+
+static const struct pasco2_transfer_function pasco2_i2c_transfer_fn = {
+	.read_data = pasco2_i2c_read_data,
+	.read_reg = pasco2_i2c_read_reg,
+	
+};
+
+int pasco2_i2c_init(const struct device *dev)
+{
+	struct pasco2_data *data = dev->data;
+	const struct pasco2_config *cfg = dev->config;
+
+	data->hw_tf = &pasco2_i2c_transfer_fn;
+
+	if (!device_is_ready(cfg->bus.bus)) {
+		return -ENODEV;
+	}
+
+	return 0;
+}
+#endif /* DT_ANY_INST_ON_BUS_STATUS_OKAY(i2c) */
 
 LOG_MODULE_REGISTER(PASCO2, CONFIG_SENSOR_LOG_LEVEL);
-
-// static int pasco2_read_mode(const struct device *dev)
-// {
-// 	const struct pasco2_config *cfg = dev->config;
-// 	uint8_t chip_id
-// 	int ret;
-
-// 	ret = data->hw_tf->read_reg(dev, PASCO2_PROD_ID, &chip_id);
-// 		if (ret) {
-// 			LOG_DBG("%s: Failed to read %s: %d", dev->name, "chip id", ret);
-// 			return ret;
-// 		}
-
-// 		if (chip_id != PASCO2_PROD_ID) {
-// 			LOG_DBG("%s: Invalid chip id 0x%x", dev->name, chip_id);
-// 			return -EIO;
-// 		}
-// 		return ret;
-// 	}
 
 static int pasco2_write_command(const struct device *dev, uint8_t cmd)
 {
 	const struct pasco2_config *cfg = dev->config;
-	uint8_t tx_buf[1] = { cmd };
+	uint8_t tx_buf[2] = { cmd };
 
 	return i2c_write_dt(&cfg->bus, tx_buf, sizeof(tx_buf));
 }
 int pasco2_write_reg(const struct device *dev, uint16_t cmd, uint16_t val)
 {
 	const struct pasco2_config *config = dev->config;
-	uint8_t tx_buf[5];
+	uint8_t tx_buf[6];
 
 	sys_put_be16(cmd, &tx_buf[0]);
-	sys_put_be16(val, &tx_buf[2]);
+	// sys_put_be16(val, &tx_buf[2]);
 
 	return i2c_write_dt(&config->bus, tx_buf, sizeof(tx_buf));
 }
 
-// static int pasco2_read_sample(const struct device *dev,
-// 		uint16_t *co2ppm_sample)
-// {
-// 	const struct pasco2_config *cfg = dev->config;
-// 	uint8_t rx_buf[6];
-// 	int rc;
-
-// 	rc = i2c_read_dt(&cfg->bus, rx_buf, sizeof(rx_buf));
-// 	if (rc < 0) {
-// 		LOG_ERR("Failed to read data from device.");
-// 		return rc;
-// 	}
-// 		*co2ppm_sample = sys_get_be16(rx_buf);
-	
-// 	return 0;
-// }
-
 static int pasco2_sample_fetch(const struct device *dev,
 			       enum sensor_channel chan)
 {
-	const struct pasco2_config *config = dev->config;
+	// const struct pasco2_config *config = dev->config;
 	struct pasco2_data *data = dev->data;
-	uint8_t rx_buf[2];
-	uint16_t co2ppm_sample;
-
+	uint8_t rx_buf[6];
+	uint8_t co2ppm_sample;
+	
 	__ASSERT_NO_MSG(chan == SENSOR_CHAN_ALL);
-
-	// if (i2c_read_dt(&config->bus, rx_buf, sizeof(rx_buf)) < 0) {
-	// 	LOG_DBG("Failed to read data sample!");
-	// 	return -EIO;
-	// }
-	// co2ppm_sample = sys_get_be16(&rx_buf[0]);
+	co2ppm_sample = sys_get_be16(&rx_buf[2]);
 	data->co2ppm_sample = co2ppm_sample;
-
-	// rc = pasco2_write_command(dev, measure_cmd[cfg->repeatability]);
-	// if (rc < 0) {
-	// 	LOG_ERR("Failed to start measurement.");
-	// 	return rc;
-	// }
-
-	// k_sleep(K_USEC(measure_wait_us[cfg->repeatability]));
-
-	// pasco2_read_sample(dev, &data->co2ppm_sample, &data->co2ppm_sample);
-	// if (rc < 0) {
-	// 	LOG_ERR("Failed to fetch data.");
-	// 	return rc;
-	// }
-
+	// LOG_INF("CO2; %u",data->co2ppm_sample);
 	return 0;
+	
 }
 
 static int pasco2_channel_get(const struct device *dev,
 			      enum sensor_channel chan,
 			      struct sensor_value *val)
 {
-	const struct pasco2_data *data = dev->data;
-	uint64_t co2ppm;
+	// const struct pasco2_config *config = dev->config;
+	struct pasco2_data *data = dev->data;
+	uint8_t co2ppm;
 
 	
 	if (chan == SENSOR_CHAN_CO2) {
 	
-	co2ppm = (uint64_t)data->co2ppm_sample * 1;
-	val->val1 = (int32_t)(co2ppm * 1000) ;
-	val->val2 = co2ppm/1000;
+	co2ppm = (uint8_t)data->co2ppm_sample ;
+	val->val1 = (uint8_t)(co2ppm) ;
 	}
 	else {
 		return -ENOTSUP;
@@ -136,7 +120,6 @@ static int pasco2_init(const struct device *dev)
 		LOG_ERR("I2C bus %s is not ready!", cfg->bus.bus->name);
 		return -EINVAL;
 	}
-	
 
 	if (pasco2_write_command(dev, PASCO2_SENS_RST) < 0) {
 		LOG_DBG("Failed to clear status register!");
@@ -155,13 +138,13 @@ static const struct sensor_driver_api pasco2_driver_api ={
 
 };
 
-#define  PASCO2_DEFINE(inst)												\
+#define  PASCO2_DEFINE(inst)											\
 	struct  pasco2_data  pasco20_data_##inst;							\
 	static const struct  pasco2_config  pasco20_cfg_##inst = {			\
 		.bus = I2C_DT_SPEC_INST_GET(inst)								\
 		};																\
 	DEVICE_DT_INST_DEFINE(inst,  pasco2_init, NULL,						\
-		&pasco20_data_##inst, &pasco20_cfg_##inst,					\
+		&pasco20_data_##inst, &pasco20_cfg_##inst,						\
 		POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY,						\
 		&pasco2_driver_api);
 
--- samples/sensor/pasco2/src/main.c
+++ samples/sensor/pasco2/src/main.c
@@ -11,6 +11,8 @@
 #include <stdio.h>
 
 void main(void){
+//   const struct i2c_device_id * id_table;
+
     const struct device *dev = device_get_binding("PASCO2");
     int rc;
 
@@ -30,62 +32,14 @@ void main(void){
             printf("PASCO2: failed: %d\n", rc);
             break;
         }
-        printf("PASCO2; %.2f ppm\n",sensor_value_to_double(&co2ppm));
+        // co2= sensor_value_to_double(&co2ppm);
+        // printf("I2C Port    : %d \n",id_table);
+        printf("CO2 : %.2f ppm\n",sensor_value_to_double(&co2ppm));
+        // printf("PASCO2: %.2f ppm\n",co2);
+        // readc= pasco2_read_mode(dev, );
+        // printf("PASCO2 Prod ID: %#02x \n",readc);
+
         k_sleep(K_MSEC(2000));
        
 	}
     }
-// }
-
-// #include <zephyr.h>
-// #include <device.h>
-// #include <drivers/sensor.h>
-// #include <sys/printk.h>
-// #include <sys/util.h>
-
-// struct channel_info {
-// 	int chan;
-// 	char *dev_name;
-// };
-
-// static struct channel_info info[] = {
-// 	{ SENSOR_CHAN_CO2, "PASCO2" }
-// };
-
-// void main(void)
-// {
-// 	const struct device *dev[ARRAY_SIZE(info)];
-// 	struct sensor_value val[ARRAY_SIZE(info)];
-// 	unsigned int i;
-// 	int rc;
-
-// 	for (i = 0; i < ARRAY_SIZE(info); i++) {
-// 		dev[i] = device_get_binding(info[i].dev_name);
-// 		if (dev[i] == NULL) {
-// 			printk("Failed to get \"%s\" device\n",
-// 			       info[i].dev_name);
-// 			return;
-// 		}
-// 	}
-
-//     while (1) {
-// 		/* fetch sensor samples */
-// 		for (i = 0U; i < ARRAY_SIZE(info); i++) {
-// 			rc = sensor_sample_fetch(dev[i]);
-// 			if (rc) {
-// 				printk("Failed to fetch sample for device %s (%d)\n",
-// 				       info[i].dev_name, rc);
-// 			}
-// 		}
-
-// 		for (i = 0U; i < ARRAY_SIZE(info); i++) {
-// 			rc = sensor_channel_get(dev[i], info[i].chan, &val[i]);
-// 			if (rc) {
-// 				printk("Failed to get data for device %s (%d)\n",
-// 				       info[i].dev_name, rc);
-// 				continue;
-// 			}
-// 		}
-//         k_sleep(K_MSEC(2000));
-// 	}
-// }
