ebfd27acb4c73d362ab5f2932cc19ba53cda1ee0
Aiman Mazlan <105735429+llyn28@users.noreply.github.com>
2023-01-30 09:04:07 +0800

    Revert "Revert "add state under button_pressed func""
    This reverts commit 4457ec3103344286988f889e76790d485fe04f33.

--- samples/basic/test_prog/src/main.c
+++ samples/basic/test_prog/src/main.c
@@ -5,106 +5,104 @@
 #include <sys/printk.h>
 #include <inttypes.h>
 
-#define SLEEP_TIME_MS	1
+#define SLEEP_TIME_MS   1000
 
-
-#define SW0_NODE	DT_ALIAS(sw0)
 #define LED0_NODE	DT_ALIAS(led0)
-#if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
-#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
-#error "Unsupported board: sw0 devicetree alias is not defined"
-#endif
-
-#define LED0	""
-#define PIN	0
-#define FLAGS	0
-#endif
+#define SW0_NODE	DT_ALIAS(sw0)
 
 enum states {
     STATE_LED_OFF,
     STATE_LED_ON
 };
 
-static struct device *led_dev;
-static struct device *button_dev;
+//const struct device *led_dev;
+//const struct device *button_dev;
 
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+static struct gpio_callback button_cb;
+static struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
-							      {0});
-static struct gpio_callback button_cb_data;
-
-static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios,
-						     {0});
-
-
-static void button_pressed(const struct device *dev, struct gpio_callback *cb,
-		    uint32_t pins)
+static void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
-	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+	printk("Button pressed\n");
 
-	    static int state = STATE_LED_OFF;
+	int val = gpio_pin_get_dt(&button);
+	static int state = STATE_LED_OFF;
 
-	if (state == STATE_LED_OFF)
+	printk("state : %d\n", state);
+
+	if (val != state)
 	{
-		gpio_pin_set(led_dev, LED_GPIO_PIN, 1);
+		gpio_pin_set_dt(&led, 0);
 		state = STATE_LED_ON;
-	} else
-	{
-		gpio_pin_set(led_dev, LED_GPIO_PIN, 0);
-		state = STATE_LED_OFF;
-
+		printk("LED OFF\n");
 	}
-}
+	else
+	{
+		gpio_pin_set_dt(&led, 1);
+		state = STATE_LED_OFF;
+		printk("LED ON\n");
+	}
 
+}
 void main(void)
 {
 	int ret;
 
-	if (!device_is_ready(button.port)) {
+	if (!device_is_ready(button.port))
+	{
 		printk("Error: button device %s is not ready\n",
-		       button.port->name);
+		button.port->name);
 		return;
 	}
 
 	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-	if (ret != 0) {
+	if (ret != 0)
+	{
 		printk("Error %d: failed to configure %s pin %d\n",
-		       ret, button.port->name, button.pin);
+		ret, button.port->name, button.pin);
 		return;
 	}
 
-	ret = gpio_pin_interrupt_configure_dt(&button,
-					      GPIO_INT_EDGE_TO_ACTIVE);
+	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
 	if (ret != 0) {
 		printk("Error %d: failed to configure interrupt on %s pin %d\n",
-			ret, button.port->name, button.pin);
+		ret, button.port->name, button.pin);
 		return;
 	}
 
-	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
-	gpio_add_callback(button.port, &button_cb_data);
+	gpio_init_callback(&button_cb, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb);
 	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
 
-	if (led.port && !device_is_ready(led.port)) {
+	if (led.port && !device_is_ready(led.port))
+	{
 		printk("Error %d: LED device %s is not ready; ignoring it\n",
 		       ret, led.port->name);
 		led.port = NULL;
 	}
-	if (led.port) {
+
+	if (led.port)
+	{
 		ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT);
-		if (ret != 0) {
+		if (ret != 0)
+		{
 			printk("Error %d: failed to configure LED device %s pin %d\n",
 			       ret, led.port->name, led.pin);
 			led.port = NULL;
-		} else {
+		} else
+		{
 			printk("Set up LED at %s pin %d\n", led.port->name, led.pin);
 		}
 	}
-
 	printk("Press the button\n");
-	if (led.port) {
-		while (1) {
-			k_sleep(K_FOREVER);
+	//gpio_pin_enable_callback(button_cb, &button);
+
+	if (led.port)
+	{
+		while (1)
+		{
+			k_msleep(SLEEP_TIME_MS);
 		}
 	}
 }
