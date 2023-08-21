73e3630bad2dd355ea1e625857ce43b81002a7ac
syafiqq <syafiq@gtsb.com.my>
2023-08-21 15:31:17 +0800

    Update mhz19b.c

--- drivers/sensor/mhz19b/mhz19b.c
+++ drivers/sensor/mhz19b/mhz19b.c
@@ -99,11 +99,14 @@ static inline int mhz19b_send_config(const struct device *dev, const enum mhz19b
 static inline int mhz19b_poll_data(const struct device *dev, const enum mhz19b_cmd_idx cmd_idx)
 {
 	struct mhz19b_data *data = dev->data;
+	const struct mhz19b_cfg *cfg = dev->config;
 	uint8_t checksum;
 	int ret;
 	int retry_count = 0;
+
 retry:
 	(void)k_mutex_lock(&co2_mutex, K_FOREVER);
+// mhz19b_uart_flush(cfg->uart_dev);
 	ret = mhz19b_send_cmd(dev, cmd_idx, true);
 	if (ret < 0 ) {
 		retry_count++;
@@ -299,14 +302,14 @@ static void mhz19b_uart_isr(const struct device *uart_dev, void *user_data)
 	if (uart_irq_rx_ready(uart_dev)) {
 		data->xfer_bytes += uart_fifo_read(uart_dev, &data->rd_data[data->xfer_bytes],
 						   MHZ19B_BUF_LEN - data->xfer_bytes);
-		if(data->rd_data[0] !=0xff)
-			{
-				data->xfer_bytes =0;	
-			}
-		else if (data->xfer_bytes == MHZ19B_BUF_LEN) {
+		if(data->rd_data[0] != 0xFF)
+		{
+			data->xfer_bytes =0;
+		}
+		 if (data->xfer_bytes == MHZ19B_BUF_LEN) {
 			data->xfer_bytes = 0;
 			
-			LOG_HEXDUMP_INF(&data->rd_data, sizeof(data->rd_data), "RX DATA:");
+			// LOG_HEXDUMP_INF(&data->rd_data, sizeof(data->rd_data), "RX DATA:");
 
 			uart_irq_rx_disable(uart_dev);
 			k_sem_give(&data->rx_sem);
@@ -322,11 +325,12 @@ static void mhz19b_uart_isr(const struct device *uart_dev, void *user_data)
 				       MHZ19B_BUF_LEN - data->xfer_bytes);
 
 		if (data->xfer_bytes == MHZ19B_BUF_LEN) {
-			LOG_HEXDUMP_INF(&mhz19b_cmds[data->cmd_idx], sizeof(mhz19b_cmds[data->cmd_idx]), "TRANFERDATA:");
+			// LOG_HEXDUMP_INF(&mhz19b_cmds[data->cmd_idx], sizeof(mhz19b_cmds[data->cmd_idx]), "TRANFERDATA:");
 			data->xfer_bytes = 0;
 			uart_irq_tx_disable(uart_dev);
+			k_sem_give(&data->tx_sem);
 			// if (!data->has_rsp) {
-				k_sem_give(&data->tx_sem);
+			// 	k_sem_give(&data->tx_sem);
 			// }
 		}
 	}
